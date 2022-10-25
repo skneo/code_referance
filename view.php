f684f1fffb07fa4f73584b828fd367b0efd1a5cb
Satish Kushwah <satishkushwahdigital@gmail.com>
2022-10-25 08:08:04 -0700

    set ref file title

--- view.php
+++ view.php
@@ -5,6 +5,7 @@ include 'header.php';
     <a href='index.php' class='btn btn-primary btn-sm mb-2'>&larr; Back</a>
     <?php
     $refFile = $_GET['ref'];
+    echo "<script>document.title='". strtoupper($refFile) ." Referance'</script>;";
     echo "<h3 class='text-success'><u>" . strtoupper($refFile) . " Referance</u></h3>";
     if (isset($_SESSION['ref_login'])) {
         echo "<a href='edit.php?editRef=$refFile' class='btn btn-sm btn-outline-primary'>Edit</a>";
