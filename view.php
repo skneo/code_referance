d5541342e7e5f1fabc9a5bd7b668d0464ec18151
Satish Kushwah <satishkushwahdigital@gmail.com>
2022-11-01 16:09:04 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,5 +1,15 @@
 <?php
 include 'header.php';
+if (isset($_POST['editRef']) and isset($_SESSION['ref_login'])) {
+    $editRef = $_POST['editRef'];
+    $refData=file_get_contents('ref/' . $editRef . '.txt');
+    file_put_contents('ref-old/' . $editRef . '.txt', $refData);
+    $refData = $_POST['refData'];
+    file_put_contents('ref/' . $editRef . '.txt', $refData);
+    echo "<div class='alert alert-success' role='alert'>
+                Referance file edited of $editRef
+                </div>";
+}
 ?>
 <div class="container my-3">
     <a href='index.php' class='btn btn-primary btn-sm mb-2'>&larr; Back</a>
